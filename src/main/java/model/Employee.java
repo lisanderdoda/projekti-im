24d2b7976abc637abd055a908afb145ff8af45f1
Armandokodheli <armandokodheli10@hotmail.com>
2021-01-17 03:42:07 -0800

    create employee and order Item class

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,42 @@
+package model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String role;
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+    private String username;
+    private String password;
+    @Column(name = "created_by")
+    private Integer createdBy;
+    @Column(name = "modified_by")
+    private Integer modifiedBy;
+    @Column(name = "modified_on")
+    private LocalDate modifiedOn;
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+    @Column(name = "created_on")
+    private LocalDate createdOn;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+}
