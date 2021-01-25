521f538fc70af21073b12d9c9f7baee8e06eea93
Armandokodheli <armandokodheli10@hotmail.com>
2021-01-25 22:48:11 +0100

    create method add,edit,remove for OrderItemRepository, MenuItemRepository

--- src/main/java/repository/MenuItemRepository.java
+++ src/main/java/repository/MenuItemRepository.java
@@ -2,16 +2,36 @@ package repository;
 
 import model.MenuItem;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import util.HibernateUtils;
+
 import javax.persistence.Query;
 import java.util.List;
 
 
 public class MenuItemRepository {
 
-   public void addMenuItem(MenuItem menuItem){}
-   public void editMenuItem(MenuItem menuItem){}
-   public void removeMenuItem(MenuItem menuItem){}
+   public void addMenuItem(MenuItem menuItem){
+       Session session=HibernateUtils.getSessionFactory().openSession();
+       Transaction transaction=session.beginTransaction();
+       session.save(menuItem);
+       transaction.commit();
+       session.close();
+
+   }
+   public void editMenuItem(MenuItem menuItem){
+       Session session=HibernateUtils.getSessionFactory().openSession();
+       Transaction transaction=session.beginTransaction();
+       session.update(menuItem);
+       transaction.commit();
+       session.close();
+   }
+   public void removeMenuItem(MenuItem menuItem){
+       Session session=HibernateUtils.getSessionFactory().openSession();
+       Transaction transaction=session.beginTransaction();
+       session.save(menuItem);
+       session.close();;
+   }
    public List<MenuItem> showMenu(){
         Session session = HibernateUtils.getSessionFactory().openSession();
         Query query1 = session.createQuery("from MenuItem m where m.isDeleted=false ");
--- src/main/java/repository/OrderItemRepository.java
+++ src/main/java/repository/OrderItemRepository.java
@@ -1,11 +1,29 @@
 package repository;
 
 import model.OrdersItems;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtils;
 
 
 public class OrderItemRepository {
-   public void addOrderItem(OrdersItems ordersItems){}
-   public void editOrderItem(OrdersItems ordersItems){}
-   public void removeOrderItem(OrdersItems ordersItems){}
+   public void addOrderItem(OrdersItems ordersItems){
+      Session session= HibernateUtils.getSessionFactory().openSession();
+      Transaction transaction=session.beginTransaction();
+      session.save(ordersItems);
+      session.close();
+   }
+   public void editOrderItem(OrdersItems ordersItems){
+      Session session=HibernateUtils.getSessionFactory().openSession();
+      Transaction transaction=session.beginTransaction();
+      session.update(ordersItems);
+      session.close();
+   }
+   public void removeOrderItem(OrdersItems ordersItems){
+      Session session=HibernateUtils.getSessionFactory().openSession();
+      Transaction transaction=session.beginTransaction();
+      session.save(ordersItems);
+      session.close();
+   }
 
 }
